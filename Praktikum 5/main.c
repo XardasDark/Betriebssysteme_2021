46eaa04736fb44d5a313e71328eacb884c661111
BeckiLukas <73895024+BeckiLukas@users.noreply.github.com>
2021-05-27 17:22:03 +0200

    implement 3 Scheduling funktions

--- Praktikum 5/main.c
+++ Praktikum 5/main.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 #include "lists.h"
+
+
+
+
 typedef struct Job
 {
     LIST_NODE_HEADER(struct Job);
@@ -16,16 +20,41 @@ typedef struct
     LIST_HEADER(JobNode);
 } JobList;
 
+int compareJobPrio(JobNode *job1, JobNode *job2, void *userData){
+    if (job1->priority < job2->priority){
+        return 1;
+    }else if(job1->priority >  job2->priority){
+        return -1;
+    }else{
+        return 0;
+    }
+}
+
+int compareJobProcessingTime(JobNode *job1, JobNode *job2, void *userData){
+     if (job1->processingTime < job2->processingTime){
+        return -1;
+    }else if(job1->processingTime >  job2->processingTime){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+
 
 
 static JobList list;
+int currTime;
+int residenceTime;
+int listLength;
+double averageResidenceTime;
 
-int main(void)
-{
-    // Liste initialisieren
+void jobListIni(){
+     // Liste initialisieren
     List_init(&list);
 
-    // Studenten erstellen
+    // Jobs erstellen
     JobNode *a = LIST_NEW_NODE(JobNode);
     a->name =  "A";
     a->processingTime = 30;
@@ -51,30 +80,104 @@ int main(void)
     e->processingTime = 18;
     e->priority = 3;
 
-    // Studenten zur Liste hinzufügen
+    // Jobs zur Liste hinzufügen
     List_append(&list, a);
     List_append(&list, b);
     List_append(&list, c);
     List_append(&list, d);
     List_append(&list, e);
+}
 
-    
-    List_remove(&list, b);
-    printf("Anzahl der Studenten: %ld", List_count(&list));
+void firstComeFirstServed(){
+    currTime = 0;
+    residenceTime = 0;
+    listLength = List_count(&list);
+    int i = 0;
 
-    
-
-    // Jedes Element der Liste ausgeben
     JobNode *current = list.head;
-    while (current != NULL)
-    {
-        printf("%s - %d\n", current->name, current->processingTime);
+    printf("Start (Aktuelle Zeit: %d).\n", currTime);
+    while(current != NULL){
+        currTime += current->processingTime;
+        residenceTime += (listLength-i) * current->processingTime;
+        printf("%s wurde abgearbeitet (Aktuelle Zeit: %d).\n", current->name, currTime);
+        JobNode *prev = current;
         current = current->next;
+        List_remove(&list, prev);
+        i++;
     }
- 
-    // Alle Nodes löschen (reicht nur für Nodes, die keine Pointer 
-    // auf allozierten Speicher besitzen)
-    List_done(&list, NULL, NULL);
+
+    averageResidenceTime = (double)residenceTime / listLength;
+    printf("Mittlere Verweilzeit: %.2f\n\n", averageResidenceTime);
+}
+
+void shortestJobFirst(){
+    List_sort(&list, (ListNodeCompareFunction) compareJobProcessingTime, NULL);
+    firstComeFirstServed();
+
+}
+
+void prioScheduling(){
+    List_sort(&list, (ListNodeCompareFunction) compareJobPrio, NULL);
+    firstComeFirstServed();
+
+}
+
+void roundRobin(){
+    currTime = 0;
+    residenceTime = 0;
+    listLength = List_count(&list);
+    int parts = 0;
+    while(!List_isEmpty(&list)){
+        parts ++;
+        printf("Es wird an den Jobs zu folgenden Anteilen gearbeitet (Aktuelle Zeit: %d).\n", currTime);
+
+        JobNode *current = list.head;
+        while(current != NULL){
+            currTime++;
+            current->processingTime --;
+            printf("Es wurde %ds an %s gearbeitet.", parts, current->name);
+            if(current->processingTime == 0){
+                residenceTime += currTime;
+                printf(" | %s wurde abgearbeitet.\n", current->name);
+                JobNode *prev = current;
+                current = current->next;
+                List_remove(&list, prev);
+            }else{
+                printf("\n");
+                current = current->next;
+            }
+        }
+        printf("\n");
+    }
+    printf("Alle Jobs abgearbeitet (Aktuelle Zeit: %d).\n\n", currTime);
+    averageResidenceTime = (double)residenceTime / listLength;
+    printf("Mittlere Verweilzeit: %.2f\n\n", averageResidenceTime);
+
+}
+
+void roundRobinPrio(){
+
+}
+
+
+
+int main(void)
+{
+   
+    jobListIni();
+    firstComeFirstServed();
+
+    jobListIni();
+    shortestJobFirst();
+
+    jobListIni();
+    prioScheduling();
+
+    jobListIni();
+    roundRobin();
+   
+    
+    
     return 0;
 }
 
